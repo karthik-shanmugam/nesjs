3041450ade7cba5a93fdf3a94d4e66022f16c825
karthik-shanmugam <karthiks@berkeley.edu>
2015-05-28 15:10:15 -0700

    implement all cpu instructions

--- cpu.js
+++ cpu.js
@@ -3,8 +3,8 @@ var A = 0;
 var X = 0;
 var Y = 0;
 var P = 0;
-var PC = 0x600;
-var SP = 0xff;
+var PC = 0x8000 - 1;
+var SP = 0x1FF;
 
 
 
@@ -130,6 +130,12 @@ function processOpcode(opcode) {
 	}
 }
 
+
+
+
+
+// Addition/Subtraction
+// TODO BCD Mode?
 // why is addition so complicated Q_Q
 function ADC(M) {
 	var sameSign = ~((A >>> 7) ^ (M >>> 7));
@@ -143,36 +149,22 @@ function ADC(M) {
 	setSignFlag(A >>> 7);
 	setZeroFlag(~A);
 }
-function AND(M) {
-	A &= M;
-	setSignFlag(A >> 7);
-	setZeroFlag(~A);
+// TODO
+function SBC(val) {
+
 }
 
-function ASLA() {
-	A <<= 1;
-	setCarryFlag(A >> 8);
-	A &= 0xFF;
-	setZeroFlag(~A);
-	setSignFlag(A >> 7);
-}
-function ASLM(addr) {
-	var temp = readMemory(addr);
-	temp <<= 1;
-	setCarryFlag(temp >> 8);
-	temp &= 0xFF;
-	setZeroFlag(~temp);
-	setSignFlag(temp >> 7);
-	writeMemory(addr, temp);
-}
 
-function BIT(M) {
-	var temp = A & M;
-	setZeroFlag(~temp);
-	setSignFlag(temp >> 7);
-	setOverflowFlag(temp >> 6);
-}
 
+
+
+
+
+
+
+
+// branching operations
+// TODO timing when crossing page?
 function branch(val) {
 	if (val >> 7) {
 		PC -= (~val + 1) & 0xFF;
@@ -180,9 +172,6 @@ function branch(val) {
 		PC += val;
 	}
 }
-
-
-
 function BCS(val) {
 	if (getCarryFlag()) {
 		branch(val);
@@ -224,6 +213,15 @@ function BVC(val) {
 	}
 }
 
+
+
+
+
+
+
+
+
+// comparisons
 function CMP(val) {
 	setCarryFlag(A >= val);
 	setZeroFlag(A == val);
@@ -242,20 +240,26 @@ function CPY(val) {
 	setSignFlag((Y - val) < 0);
 }
 
+
+
+
+
+
+
+
+// Increment/Decrement operations
 function DEC(addr) {
 	var temp = readMemory(addr);
 	temp = temp - 1;
 	setSignFlag(temp < 0);
 	setZeroFlag(temp == 0);
-
 	// need to do this hack to deal with 8 bit vs 32 bit numbers
 	if (temp < 0) {
 		temp &= 0x7F;
 		temp |= 1 << 7;
 	}
-	writeMemory(addr);
+	writeMemory(addr, temp);
 }
-
 function DEX(val) {
 	X = X - 1;
 	setSignFlag(X < 0);
@@ -267,7 +271,6 @@ function DEX(val) {
 		X |= 1 << 7;
 	}
 }
-
 function DEY(val) {
 	Y = Y - 1;
 	setSignFlag(Y < 0);
@@ -279,6 +282,156 @@ function DEY(val) {
 		Y |= 1 << 7;
 	}
 }
+// incrementing doesn't involve fudging around with two's complement stuff
+function INC(addr) {
+	var temp = readMemory(addr);
+	temp = temp + 1;
+	temp &= 0xFF;
+	setSignFlag(temp >> 7);
+	setZeroFlag(~temp);
+	writeMemory(addr, temp);
+}
+function INX() {
+	X = X + 1;
+	X &= 0xFF;
+	setSignFlag(X >> 7);
+	setZeroFlag(~X);
+}
+function INY() {
+	Y = Y + 1;
+	Y &= 0xFF;
+	setSignFlag(Y >> 7);
+	setZeroFlag(~Y);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// JUMPING AROUND AND STUFF
+// TODO
+function BRK() {
+}
+function JMP(val) {
+	PC = val;
+}
+// TODO
+function JSR(val) {
+}
+// TODO check if we should be pulling MSByte of PC first
+function RTI() {
+	P = pull();
+	PC = (pull() << 8) | pull();
+}
+function RTS() {
+	PC = (pull() << 8) | pull();
+	PC--;
+}
+
+
+
+
+
+
+
+
+
+// BIT OPERATIONS
+// some functions are four lettered; these can operate on Acc or a memory locaiton
+// this is fundamentally different from most functions using memory 
+// because it reads and writes and therefore needs special implementation.
+
+function BIT(M) {
+	var temp = A & M;
+	setZeroFlag(~temp);
+	setSignFlag(temp >> 7);
+	setOverflowFlag(temp >> 6);
+}
+function ASLA() {
+	A <<= 1;
+	setCarryFlag(A >> 8);
+	A &= 0xFF;
+	setZeroFlag(~A);
+	setSignFlag(A >> 7);
+}
+function ASLM(addr) {
+	var temp = readMemory(addr);
+	temp <<= 1;
+	setCarryFlag(temp >> 8);
+	temp &= 0xFF;
+	setZeroFlag(~temp);
+	setSignFlag(temp >> 7);
+	writeMemory(addr, temp);
+}
+
+function LSRA() {
+	setCarryFlag(A & 1);
+	A >>>= 1;
+	setSignFlag(0);
+	setZeroFlag(~A);
+}
+
+function LSRM(addr) {
+	var temp = readMemory(addr);
+	setCarryFlag(temp & 1);
+	temp >>>= 1;
+	setSignFlag(0);
+	setZeroFlag(~temp);
+	writeMemory(addr, temp);
+}
+
+function ROLA() {
+	A <<= 1;
+	A |= getCarryFlag();
+	setCarryFlag(A >>> 8);
+	A &= 0xFF;
+	setSignFlag(A >> 7);
+	setZeroFlag(~A);
+}
+
+function ROLM(addr) {
+	var temp = readMemory(addr);
+	temp <<= 1;
+	temp |= getCarryFlag();
+	setCarryFlag(temp >>> 8);
+	temp &= 0xFF;
+	setSignFlag(temp >>> 7);
+	setZeroFlag(~temp);
+	writeMemory(addr,temp);
+}
+
+function RORA() {
+	A |= getCarryFlag() << 8;
+	setCarryFlag(A & 1);
+	A >>>= 1;
+	A &= =0xFF;
+	setSignFlag(A >>> 7);
+	setZeroFlag(~A);
+}
+
+function RORM(addr) {
+	var temp = readMemory(addr);
+	temp |= getCarryFlag() << 8;
+	setCarryFlag(temp & 1);
+	temp >>>= 1;
+	temp &= =0xFF;
+	setSignFlag(temp >>> 7);
+	setZeroFlag(~temp);
+	writeMemory(addr,temp);
+}
+
+function AND(M) {
+	A &= M;
+	setSignFlag(A >> 7);
+	setZeroFlag(~A);
+}
 
 function EOR(val) {
 	A ^= val;
@@ -286,5 +439,160 @@ function EOR(val) {
 	setSignFlag(A >>> 7);
 }
 
+function ORA(val) {
+	A |= val;
+	A &= 0xFF;
+	setSignFlag(A >> 7);
+	setZeroFlag(~A);
+}
 
 
+
+
+
+
+
+
+
+function push(val) {
+	SP -= 1;
+	if (SP < 0x100) {
+		SP = 0x1FF;
+	}
+	writeMemory(SP, val);
+}
+
+function pull() {
+	var result = readMemory(SP);
+	SP = SP + 1;
+	if (SP > 0x1FF) {
+		SP = 0x100;
+	}
+	return result;
+}
+
+// stack operations
+function PHA() {
+	push(A);
+}
+
+function PHP() {
+	push(P);
+}
+
+function PLA() {
+	A = pull();
+	setSignFlag(A >>> 7);
+	setZeroFlag(~A);
+}
+
+function PLP() {
+	P = readMemory();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Status flag instructions (set/clear)
+function SEC() {
+	setCarryFlag(1);
+}
+function SED() {
+	setDecimalModeFlag(1);
+}
+function SEI() {
+	setInterruptFlag(1);
+}
+function CLC() {
+	setCarryFlag(0);
+}
+function CLD() {
+	setDecimalModeFlag(0);
+}
+function CLI() {
+	setInterruptFlag(0);
+}
+function CLV() {
+	setOverflowFlag(0);
+}
+
+
+
+
+// memory operations (store / load)
+function STA(addr) {
+	A = readMemory(addr);
+}
+function STX(addr) {
+	X = readMemory(addr);
+}
+function STY(addr) {
+	Y = readMemory(addr);
+}
+function LDA(val) {
+	A = val;
+	setZeroFlag(~A);
+	setSignFlag(A >> 7);
+}
+function LDX(val) {
+	X = val;
+	setZeroFlag(~X);
+	setSignFlag(X >> 7);
+}
+function LDY(val) {
+	Y = val;
+	setZeroFlag(~Y);
+	setSignFlag(Y >> 7);
+}
+
+
+
+// transfer operations (moving stuffz between registerz)
+function TAX() {
+	X = A;
+	setSignFlag(X >> 7);
+	setZeroFlag(~X);
+}
+function TAY() {
+	Y = A;
+	setSignFlag(Y >> 7);
+	setZeroFlag(~Y);
+}
+function TSX() {
+	X = SP;
+	setSignFlag(X >> 7);
+	setZeroFlag(~X);
+}
+function TXA() {
+	A = X;
+	setSignFlag(A >> 7);
+	setZeroFlag(~A);
+}
+function TXS() {
+	SP = X;
+}
+function TYA() {
+	A = Y;
+	setSignFlag(A >> 7);
+	setZeroFlag(~A);
+}
+
+
+
+
+
+// self explanatory
+function NOP() {
+	// lol
+}
